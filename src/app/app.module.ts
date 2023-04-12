2b5ae386c7856baa99377410189f627a2147a969
Claudio-Olivera <claudioolivera753@gmail.com>
2023-04-12 04:35:27 -0300

    creacion de carpetas y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './Common/nav/nav.component';
+import { FooterComponent } from './Common/footer/footer.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { NosotrosComponent } from './Pages/nosotros/nosotros.component';
+import { RecorridosComponent } from './Pages/recorridos/recorridos.component';
+import { BlogComponent } from './Pages/blog/blog.component';
+import { LoginComponent } from './Auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    NosotrosComponent,
+    RecorridosComponent,
+    BlogComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
