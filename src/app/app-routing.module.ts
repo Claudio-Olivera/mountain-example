cbf5ea7e5bbd88f43472f909319bbd1ac75c65b0
Claudio-Olivera <claudioolivera753@gmail.com>
2023-04-12 08:37:06 -0300

    login comp,add images in comp,change styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { HomeComponent } from './Pages/home/home.component';
 import { BlogComponent } from './Pages/blog/blog.component';
 import { NosotrosComponent } from './Pages/nosotros/nosotros.component';
 import { RecorridosComponent } from './Pages/recorridos/recorridos.component';
+import { LoginComponent } from './Auth/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'nosotros', component:NosotrosComponent },
   { path:'recorridos', component:RecorridosComponent },
-  
+  { path:'login', component:LoginComponent }
 ];
 
 @NgModule({
